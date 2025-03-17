1645e1ca6caf94431cefb4bb4cbbf4fb89fb5d9f
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2025-03-17 07:25:52 +0700

    fix(Quisioner State): Make sure when trying to change answer is updated correctly

--- src/components/Quisioner/BooleanQuestion.jsx
+++ src/components/Quisioner/BooleanQuestion.jsx
@@ -4,8 +4,41 @@ import { FaThumbsUp } from "react-icons/fa6";
 import { FaCircleCheck } from "react-icons/fa6";
 
 export const BooleanQuestion = ({ id, question, options, type, index = 1 }) => {
-  const { userChoose } = quisionerStore();
-  console.log({ userChoose });
+  const { userBooleanResponse, userBooleanResponses } = quisionerStore();
+
+  const handleChooseAnswer = (id, optionId, score) => {
+    quisionerStore.setState((prevState) => {
+      const userBooleanResponses = [...prevState.userBooleanResponses];
+
+      const isAlreadyAnswered = userBooleanResponses.findIndex(
+        (answer) => answer.questionId === id
+      );
+
+      if (isAlreadyAnswered === -1) {
+        userBooleanResponses.push({
+          questionId: id,
+          optionId: optionId,
+          score: score,
+        });
+      } else if (isAlreadyAnswered !== -1) {
+        userBooleanResponses[isAlreadyAnswered] = {
+          questionId: id,
+          optionId,
+          score,
+        };
+      }
+
+      return {
+        userBooleanResponses,
+        userBooleanResponse: {
+          questionId: id,
+          optionId: optionId,
+          score,
+        },
+      };
+    });
+  };
+
   return (
     <section>
       <h1 className="font-bold text-lg mb-4">{question}?</h1>
@@ -14,17 +47,12 @@ export const BooleanQuestion = ({ id, question, options, type, index = 1 }) => {
           options.map((option) => (
             <button
               className={`bg-gray-200 size-24 rounded-lg flex flex-col justify-center items-center gap-2 hover:bg-gray-300 transition-colors duration-300 relative  ${
-                userChoose.optionId === option.id && "!bg-[#1b82e6] text-white"
+                userBooleanResponse.optionId === option.id &&
+                "!bg-[#1b82e6] text-white"
               }`}
-              onClick={() =>
-                quisionerStore.setState({
-                  userChoose: {
-                    questionId: id,
-                    optionId: option.id,
-                    score: option.score,
-                  },
-                })
-              }
+              onClick={() => {
+                handleChooseAnswer(id, option.id, option.score);
+              }}
             >
               <FaThumbsUp className="text-[#04ff00] size-10" />
               <p className="text-sm font-semibold">{option.title}</p>
